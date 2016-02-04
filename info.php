f9fc229796cadd0166152ef8d2be8a67e8d00255
Bianka Martinovic <github.20.cerberus@spamgourmet.com>
2016-02-04 17:42:26 +0100

    several changes, will add a changes.txt later

---
+++ info.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * WebsiteBaker Community Edition (WBCE)
+ * Way Better Content Editing.
+ * Visit http://wbce.org to learn more and to join the community.
+ *
+ * @copyright Ryan Djurovich (2004-2009)
+ * @copyright WebsiteBaker Org. e.V. (2009-2015)
+ * @copyright WBCE Project (2015-)
+ * @license GNU GPL2 (or any later version)
+ *
+ * Improvements Copyright (c) 2005-2013 Hudge, Woudloper, M. Gallas, R. Smith, C. Sommer, Frank Heyne
+ *
+ * Forked for WBCE in 2016; version starts with 3.0.0
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with your CMS, if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * For part of the DEVELOPMENT HISTORY see the help file in the backend!
+ */
+/*
+ *
+ * Changelog
+ * 	Fix
+ *  version 2.65. 03feb16 -  2.65.0 03feb16
+ *  ! update to 2.65.0
+ *	Fix XSS Vulnerabilities
+ *	Evaki
+ *
+ */
+
+// VARIABLES REQUIRED FOR WEBSITE BAKER
+$module_name			= 'Download Gallery WBCE';
+$module_directory		= 'download_gallery';
+$module_function		= 'page';
+$module_version			= '3.0.0';
+$module_platform 	    = '2.8';
+$wbce_platform 	        = '1.1';
+$module_status		    = 'beta';
+$module_author			= 'Hudge, Woudloper, M. Gallas, R. Smith, C. Sommer, F. Heyne, B. Martinovic';
+$module_license			= 'GNU General Public License'; 
+$module_description		= 'This page type is designed for making a download gallery page<br />'
+                        . 'See <a href="'.WB_URL.'/modules/download_gallery/help.php?page_id=1&amp;section_id=1">help file</a> for more information.'
+                        ;
+$module_home            = 'https://github.com/WBCE/Download-Gallery-WBCE.git';
+$module_guid            = 'a691aeef-16f7-410b-8db4-2f074aa0c791';
